85dd5b752e43ad8412b2aab5a973a93edc061280
KPVISHNUSAI <kpvishnusai1111@gmail.com>
2023-04-13 15:23:34 +0530

    update

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 var aimlBtn = document.getElementById("aiml-btn");
 
-  // Get a reference to the modal
-  var aimlModal = document.getElementById("aiml-modal");
+// Get a reference to the modal
+var aimlModal = document.getElementById("aiml-modal");
 
-  // Get a reference to the close button inside the modal
-  var aimlCloseBtn = document.getElementById("aiml-close-btn");
+// Get a reference to the close button inside the modal
+var aimlCloseBtn = document.getElementById("aiml-close-btn");
 
-  // Add an event listener to the button that will open the modal
-  aimlBtn.addEventListener("click", function() {
-    aimlModal.style.display = "block";
-  });
+// Add an event listener to the button that will open the modal
+aimlBtn.addEventListener("click", function () {
+  aimlModal.style.display = "block";
+});
 
-  // Add an event listener to the close button that will close the modal
-  aimlCloseBtn.addEventListener("click", function() {
-    aimlModal.style.display = "none";
-  });
+// Add an event listener to the close button that will close the modal
+aimlCloseBtn.addEventListener("click", function () {
+  aimlModal.style.display = "none";
+});
 
+//libvoeufsd c
