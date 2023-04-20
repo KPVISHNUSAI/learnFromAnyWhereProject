bb0f397ea852e87bedb927759e85aac12451b671
KPVISHNUSAI <kpvishnusai1111@gmail.com>
2023-04-20 19:22:21 +0530

    update

--- script.js
+++ script.js
@@ -17,4 +17,52 @@ aimlCloseBtn.addEventListener("click", function () {
 });
 
 
-//yrctyctyg
+const courses = [
+  {
+    id: 1,
+    title: "Introduction to Machine Learning",
+    videoUrl: "https://www.youtube.com/watch?v=_8fzJfUZetg",
+    thumbnailUrl: "https://i.ytimg.com/vi/_8fzJfUZetg/maxresdefault.jpg",
+    description: "This course provides an introduction to Machine Learning concepts."
+  },
+  {
+    id: 2,
+    title: "Deep Learning with TensorFlow",
+    videoUrl: "https://www.youtube.com/watch?v=JLzV2ys5nDc",
+    thumbnailUrl: "https://i.ytimg.com/vi/JLzV2ys5nDc/maxresdefault.jpg",
+    description: "This course provides an introduction to Deep Learning using TensorFlow."
+  }
+];
+
+const courseRow = document.querySelector(".course-row");
+
+function createCourseCard(course) {
+  const card = document.createElement("div");
+  card.classList.add("col-md-4");
+  card.innerHTML = `
+    <div class="card">
+      <img src="${course.thumbnailUrl}" class="card-img-top" alt="${course.title}">
+      <div class="card-body">
+        <h5 class="card-title">${course.title}</h5>
+        <p class="card-text">${course.description}</p>
+        <a href="${course.videoUrl}" class="btn btn-primary" target="_blank">Watch</a>
+      </div>
+    </div>
+  `;
+  return card;
+}
+
+courses.forEach((course) => {
+  courseRow.appendChild(createCourseCard(course));
+});
+
+// Example of adding a new course dynamically
+const newCourse = {
+  id: 3,
+  title: "Web Development with React",
+  videoUrl: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+  thumbnailUrl: "https://i.ytimg.com/vi/w7ejDZ8SWv8/maxresdefault.jpg",
+  description: "This course provides an introduction to web development using React."
+};
+
+courseRow.appendChild(createCourseCard(newCourse));
