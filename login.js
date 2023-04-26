71ef4fc61ba8d30cb495fc89fd7afff11375fe02
KPVISHNUSAI <kpvishnusai1111@gmail.com>
2023-04-26 20:00:48 +0530

    update

---
+++ login.js
@@ -0,0 +1,17 @@
+// let formEl = document.getElementById("loginId");
+// let emailEl = document.getElementById("email");
+// let passwordEl = document.getElementById("password");
+// let button = document.querySelector("btn");
+// let warningTextEl = document.getElementById("warningText");
+
+// function auth() {
+//   var email = document.getElementById("email").value;
+//   var password = document.getElementById("password").value;
+//   if (email == "kk4563@srmist.edu.in" && password == "1234") {
+//     window.location.replace("/index.html");
+//     alert("Login Successfully ");
+//   } else {
+//     alert("Invalid Information ");
+//     return;
+//   }
+// }
