69f884fc6135310578852d084adf5e09cda14cec
KPVISHNUSAI <kpvishnusai1111@gmail.com>
2023-04-28 00:21:55 +0530

    update

--- app.py
+++ app.py
@@ -13,10 +13,19 @@ def home():
         a = request.form['email']
         b = request.form['password']
         print(a,b)
-        if(a=='kpvishnusai1111@gmail.com' and b=='12345'):
+        if(a == 'kpvishnusai1111@gmail.com' and b == '12345'):
             return render_template('index.html')
         else:
             return render_template('login.html', error = 'Enter correct login crediantials')
+@app.route('/Home')
+def back():
+    return render_template('index.html')
+        
+@app.route('/btech1',methods=["GET"])
+def btech1():
+    return render_template('btech1.html')
 
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
